1f71764ac63c2d55a1ebbb8e6af8147423ab13a2
Michael Aristodemou <62950958+mearistodemou@users.noreply.github.com>
2025-11-19 16:04:13 +0100

    change data directory

--- Lecture06/ctsem_modcomp_W6.R
+++ Lecture06/ctsem_modcomp_W6.R
@@ -16,7 +16,7 @@ library(curl)
 library(parallel) # load package to detect cores
 
 # link to data on github
-url <- "https://raw.githubusercontent.com/mearistodemou/dynamic-systems-uzh/main/Lecture6/groundtruth_w6.rds"
+url <- "https://raw.githubusercontent.com/mearistodemou/dynamic-systems-uzh/main/Lecture06/groundtruth_w6.rds"
 
 # functions to load data from R
 con <- gzcon(curl::curl(url, "rb"))
